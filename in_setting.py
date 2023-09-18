22197caef2afdf9d1e02f6732fdf1212a4190dea
Pasu <89201116+Ciel289@users.noreply.github.com>
2023-09-18 15:55:23 +0700

    in_setting.py

---
+++ in_setting.py
@@ -0,0 +1,8 @@
+# คัดลอกทั้งหมดนี้ แล้วนำไปวางไว้ที่ไฟล์ setting.py
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = "" 
